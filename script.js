87c140429a8557df3ccc69858f9aa380a1349216
Mokwar <91190026+Mokwar7@users.noreply.github.com>
2023-10-22 02:05:42 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,48 +2,15 @@
 const burger = document.querySelector('.header__burger')
 const nav = document.querySelector('.header__cont')
 const body = document.querySelector('body')
-let switchHeader = false
-let toTop = 0
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('header__burger_active')
     nav.classList.toggle('header__cont_active')
-    if (switchHeader) {
-      window.scrollTo(0, toTop)
-      switchHeader = false
-    } else {
-      switchHeader = true
-      toTop = window.scrollY
-    }
 })
 
 
-//TIMER
-let countDownDate = new Date("Nov 1, 2023 00:00:00").getTime();
-
-let x = setInterval(function () {
-  let now = new Date().getTime();
-  let distance = countDownDate - now;
-
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.querySelector(".exch__day").innerHTML = days;
-  document.querySelector(".exch__hrs").innerHTML = hours;
-  document.querySelector(".exch__mins").innerHTML = minutes;
-  document.querySelector(".exch__secs").innerHTML = seconds;
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("clock").innerHTML = "Happy New Year!";
-  }
-}, 1000);
-
-
 //EXCHANGE
-let btns = document.querySelectorAll('.exch__payment')
+const btns = document.querySelectorAll('.exch__payment')
 
 btns.forEach((element) => {
   element.addEventListener('click', () => {
@@ -56,28 +23,130 @@ btns.forEach((element) => {
 
 
 //THEME
-let toggle = document.querySelector('.toggle')
-let clickedToggle = false
-let header = document.querySelector('.header')
-let logo = document.querySelector('.header__logo')
-let main = document.querySelector('.main__container')
-let team = document.querySelector('.team__container')
-let roadMap = document.querySelector('.roadmap__container')
+const toggle = document.querySelector('.header__toggle')
+const toggleDark = document.querySelector('.header__toggle-dark')
+const toggleLight = document.querySelector('.header__toggle-light')
+const clickedToggle = false
+const header = document.querySelector('.header')
+const logo = document.querySelector('.header__logo')
+const main = document.querySelector('.main__container')
+const team = document.querySelector('.team__container')
+const roadMap = document.querySelector('.roadmap')
+const what = document.querySelector('.what')
+const token = document.querySelector('.token__container')
+const whitepaperImg = document.querySelector('.whitepaperImg')
+let toggleTheme = true
 
 toggle.addEventListener('click', () => {
-  if (clickedToggle) {
-    toggle.src = './src/moon-line-icon.svg'
-    logo.style.backgroundImage = "url('./src/NAVRAS.svg')";
-    clickedToggle = false
-  } else {
-    toggle.src = './src/moon-line-icon 1.svg'
-    logo.style.backgroundImage = "url('./src/NAVRAS (2).svg')";
-    clickedToggle = true
-  }
-  toggle.classList.toggle('toggle_black')
+  toggle.classList.toggle('header__toggle_active')
+  toggleDark.classList.toggle('header__toggle-dark_active')
+  toggleLight.classList.toggle('header__toggle-light_active')
   body.classList.toggle('body_black')
   header.classList.toggle('header__container_black')
   main.classList.toggle('main__container_black')
   team.classList.toggle('team__container_black')
-  roadMap.classList.toggle('roadmap__container_black')
+  roadMap.classList.toggle('roadmap_black')
+  what.classList.toggle('what_black')
+  token.classList.toggle('token__container_black')
+  toggleTheme == true ? whitepaperImg.src = './src/whitepipe.svg' : whitepaperImg.src = './src/whitepape icon.svg'
+  toggleTheme = !toggleTheme
+})
+
+
+//SCROLL
+const mainLink = document.querySelector('.mainLink');
+const aboutLink = document.querySelector('.aboutLink')
+const tokenomicsLink = document.querySelector('.tokenomicsLink')
+const roadmapLink = document.querySelector('.roadmapLink')
+const teamLink = document.querySelector('.teamLink')
+const tokenomicsLink2 = document.querySelector('.tokenomicsLink2')
+const roadmapLink2 = document.querySelector('.roadmapLink2')
+const teamLink2 = document.querySelector('.teamLink2')
+
+mainLink.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smmoth",
+  }); 
+  main.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+tokenomicsLink2.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  token.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+roadmapLink2.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  roadMap.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+teamLink2.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  team.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+aboutLink.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  what.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+tokenomicsLink.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  token.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+roadmapLink.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  roadMap.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
+})
+
+teamLink.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "instant",
+  });
+  team.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+  burger.classList.remove('header__burger_active')
+  nav.classList.remove('header__cont_active')
 })
